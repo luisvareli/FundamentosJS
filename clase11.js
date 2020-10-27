0229e7a5e293b555a5ada913cd99e7352bf2aa73
Luis Vargas <lewiis29@gmail.com>
2020-10-27 11:07:00 -0600

    Funciones que retornan valores

---
+++ clase11.js
@@ -0,0 +1,56 @@
+var luis = {
+    nombre: 'Luis',
+    apellido: 'Vargas',
+    edad: 25,
+    ingeniero: true,
+    cocinero: false,
+    cantante: true,
+    dj:false,
+    guitarrista: false,
+    drone: false
+}
+
+var juan = {
+    nombre: 'Juan',
+    apellido: 'Gomez',
+    edad: 13,
+}
+
+function imprimirProfesiones(persona) {
+    console.log(`${persona.nombre} es:`)
+
+    if(persona.ingeniero){
+        console.log('Ingeniero')
+    } else {
+        console.log('No es Ingeniero')
+    }
+    if(persona.cocinero){
+        console.log('Cocinero')
+    }
+    if(persona.cantante){
+        console.log('Cantante')
+    }
+    if(persona.dj){
+        console.log('DJ')
+    }
+    if(persona.guitarrista){
+        console.log('Guitarrista')
+    }
+    if(persona.drone){
+        console.log('Piloto de drone')
+    }
+}
+
+const MAYORIA_DE_EDAD = 18
+
+function esMayorDeEdad(persona) {
+    return persona.edad >= MAYORIA_DE_EDAD
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if(esMayorDeEdad(persona)) {
+        console.log(`${persona.nombre} es mayor de edad`)
+    } else {
+        console.log(`${persona.nombre} es menor de edad`)
+    }
+}
