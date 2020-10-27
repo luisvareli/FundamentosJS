f5e23ccbfebb217514b6d7338a7d360dbed0504c
Luis Vargas <lewiis29@gmail.com>
2020-10-27 09:51:37 -0600

    Desestructurar objetos

---
+++ clase7.js
@@ -0,0 +1,24 @@
+var luis = {
+    nombre: 'Luis',
+    apellido: 'Vargas',
+    edad: 25
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Susnisky',
+    edad: 25
+}
+
+function imprimirNombreEnMayusculas(persona) {
+    //var nombre = persona.nombre
+    var {nombre} = persona
+    console.log(nombre.toUpperCase())
+}
+
+imprimirNombreEnMayusculas(luis)
+imprimirNombreEnMayusculas(dario)
+imprimirNombreEnMayusculas({nombre: 'Pepito'})
+
+
+
