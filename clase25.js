9b0b9afad5255dc0a4a77bdbd21514f913e0c5fc
Luis Vargas <lewiis29@gmail.com>
2020-10-27 15:32:47 -0600

    Clases en JavaScript

---
+++ clase25.js
@@ -0,0 +1,26 @@
+class Persona {
+    constructor(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+    }
+    saludar(){
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+    }
+    soyAlto(){
+        return  this.altura > 1.8
+    }
+}
+
+class Desarrollador extends Persona {
+    constructor(nombre, apellido, altura) {
+        super(nombre, apellido, altura)
+    }
+    saludar() {
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+    }
+}
+
+// var luis = new Persona('Luis', 'Vargas',1.72)
+// var erika = new Persona('Erika', 'Luna',1.65)
+// var arturo = new Persona('Arturo', 'Martinez',1.89)
