3c00fc6d54347a8df440b0041259900f070c58b2
Luis Vargas <lewiis29@gmail.com>
2020-10-27 14:33:35 -0600

    Modificando un prototipo

---
+++ clase22.js
@@ -0,0 +1,20 @@
+function Persona (nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+Persona.prototype.saludar = () => {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.soyAlto = () => this.altura > 1.8
+
+var luis = new Persona('Luis', 'Vargas',1.72)
+var erika = new Persona('Erika', 'Luna',1.65)
+var arturo = new Persona('Arturo', 'Martinez',1.89)
+
+luis.soyAlto()
+erika.soyAlto()
+arturo.soyAlto()
+
