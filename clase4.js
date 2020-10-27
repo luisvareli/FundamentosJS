7a07f80c74e9079f37dd4e0b9f9c910185fc6772
Luis Vargas <lewiis29@gmail.com>
2020-10-27 09:47:36 -0600

    Funciones

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Luis', edad = 25
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 25)
+imprimirEdad('Dario', 23)
